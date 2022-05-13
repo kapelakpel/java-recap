a4f65ab689d6e18748bc0292837f17529dfca30e
Mateusz Kapelinski <mateuszkapelinski@gmail.com>
2022-05-13 20:58:14 +0200

    Add AccoutStatus enum class.

---
+++ src/main/java/pl/kapelinski/mateusz/java/recap/app/bank/AccountStatus.java
@@ -0,0 +1,34 @@
+package pl.kapelinski.mateusz.java.recap.app.bank;
+
+public enum AccountStatus {
+    INACTIVE(1, "Nieaktywne"),
+    ACTIVE(2, "Aktywne"),
+    SUSPENDED(3, "Zawieszone"),
+    DELETED(4, "Usuniete");
+
+    private int statusNumber;
+    private String statusCode;
+
+    AccountStatus() {
+    }
+
+    AccountStatus(int statusNumber) {
+        this.statusNumber = statusNumber;
+    }
+
+    AccountStatus(int statusNumber, String statusCode) {
+        this.statusNumber = statusNumber;
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusNumber() {
+        return statusNumber;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+}
+
+// TODO: 06.05.2022
+//uzupelnic konstruktory dla pozostalych enumow - DONE
