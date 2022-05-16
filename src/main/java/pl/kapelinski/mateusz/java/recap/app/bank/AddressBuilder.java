01ec869467f7133c08b4abff156612261d6427e4
Mateusz Kapelinski <mateuszkapelinski@gmail.com>
2022-05-16 20:40:47 +0200

    Add AddresBuilder class.

---
+++ src/main/java/pl/kapelinski/mateusz/java/recap/app/bank/AddressBuilder.java
@@ -0,0 +1,38 @@
+package pl.kapelinski.mateusz.java.recap.app.bank;
+
+public class AddressBuilder {
+    private String city;
+    private String postCode;
+    private String houseNumber;
+    private String apartmentNumber;
+    private String street;
+
+    public AddressBuilder setCity(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public AddressBuilder setPostCode(String postCode) {
+        this.postCode = postCode;
+        return this;
+    }
+
+    public AddressBuilder setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+        return this;
+    }
+
+    public AddressBuilder setApartmentNumber(String apartmentNumber) {
+        this.apartmentNumber = apartmentNumber;
+        return this;
+    }
+
+    public AddressBuilder setStreet(String street) {
+        this.street = street;
+        return this;
+    }
+
+    public Address createAddress() {
+        return new Address(city, postCode, houseNumber, apartmentNumber, street);
+    }
+}
