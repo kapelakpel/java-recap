7a074cfedfb7be6b607ae2fad561da198f5f4a30
Mateusz Kapelinski <mateuszkapelinski@gmail.com>
2022-05-27 10:24:49 +0200

    Refactor package to reflect application layers.

--- src/main/java/pl/kapelinski/mateusz/java/recap/app/bank/ui/model/Transfer.java
+++ src/main/java/pl/kapelinski/mateusz/java/recap/app/bank/ui/model/Transfer.java
@@ -1,4 +1,7 @@
-package pl.kapelinski.mateusz.java.recap.app.bank;
+package pl.kapelinski.mateusz.java.recap.app.bank.ui.model;
+
+import pl.kapelinski.mateusz.java.recap.app.bank.api.exception.AccountTopUpException;
+import pl.kapelinski.mateusz.java.recap.app.bank.api.exception.AccountWithdrawException;
 
 public class Transfer {
 
