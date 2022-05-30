136fd28a5cc70efb39a8e72eb03e394a2f2a3e5f
Mateusz Kapelinski <mateuszkapelinski@gmail.com>
2022-05-30 19:52:33 +0200

    Add CRUD to controller class.

--- src/main/java/pl/kapelinski/mateusz/java/recap/app/bank/ui/controller/Controller.java
+++ src/main/java/pl/kapelinski/mateusz/java/recap/app/bank/ui/controller/Controller.java
@@ -1,4 +1,18 @@
 package pl.kapelinski.mateusz.java.recap.app.bank.ui.controller;
 
 public class Controller {
+    public void create() {
+    }
+
+    public void read() {
+    }
+
+    public void update() {
+    }
+
+    public void delete() {
+    }
+
+    public void list() {
+    }
 }
